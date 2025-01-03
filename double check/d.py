405b917e2b8888be464d5645dcab9d9ba109e590
SafwanFaruk <safwan.faruk17>
2025-01-03 22:14:15 +0600

    d

---
+++ double check/d.py
@@ -0,0 +1,10 @@
+num=int(input("enter the number:"))
+
+if num>50:
+    print("the number is greater than 50")
+else:
+    print("the number is less than 50")
+    if num%2==0:
+        print("its even number")
+    else:
+        print("its odd number")
